2f1748e28f58d125bb80ca598e12e876d44df5e5
ankit anand <ankit.anand@ankits-MacBook-Air.local>
2025-01-08 16:13:24 +0530

    fixing base url

---
+++ questions/management/commands/tester.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        from ... helpers import common_utils as helper
+        pdf_path = "/Users/ankit.anand/Desktop/upsc_pyq/2022.pdf"
+        text = helper.extract_text_from_scanned_pdf_using_gemini(pdf_path)
+        print(text)
