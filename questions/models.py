d28057856f1a6a465bb8c6d890e0fd122076f06f
ankit anand <ankit.anand@ankits-MacBook-Air.local>
2025-01-18 04:35:20 +0530

    adding database validations and updating schema with better design

--- questions/models.py
+++ questions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Subjects(models.TextChoices):
@@ -15,35 +16,45 @@ class Subjects(models.TextChoices):
 
 class PYQuestion(models.Model):
     subject = models.CharField(max_length=4, choices=Subjects.choices)
-    question_text = models.TextField()
-    year = models.PositiveIntegerField()
-    q_num = models.PositiveIntegerField()
+    question_text = models.TextField(null=False, blank=False)
+    year = models.PositiveIntegerField(null=False)
+    q_num = models.PositiveIntegerField(null=False)
+    explanation = models.OneToOneField('Explanation', on_delete=models.CASCADE, related_name='question')
 
     class Meta:
         ordering = ['year', 'q_num']
         db_table = 'prelims_pyq'
+        unique_together = ('year', 'q_num')
 
     def __str__(self):
         return self.question_text[:50] + "..."
 
+    def clean(self):
+        if self.options.count() != 4:
+            raise ValidationError("Question must have exactly 4 options.")
+
+        num_questions_per_year = PYQuestion.objects.filter(year=self.year).count()
+        if num_questions_per_year >= 100:
+            raise ValidationError("Cannot exceed 100 questions per year")
+
 
 class Options(models.Model):
     question = models.ForeignKey(PYQuestion, on_delete=models.CASCADE, related_name='options')
-    option_text = models.TextField()
-    is_correct = models.BooleanField(default=False)
-    option_num = models.PositiveIntegerField()
+    option_text = models.TextField(null=False, blank=False)
+    is_correct = models.BooleanField(default=False, null=False)
+    option_num = models.PositiveIntegerField(null=False)
 
     class Meta:
         ordering = ['option_num']
         db_table = 'prelims_options'
+        unique_together = ('question', 'option_num')
 
     def __str__(self):
         return self.option_text
 
 
 class Explanation(models.Model):
-    question = models.ForeignKey(PYQuestion, on_delete=models.CASCADE, related_name='explanation')
-    explanation_text = models.TextField()
+    explanation_text = models.TextField(blank=False, null=False)
 
     class Meta:
         db_table = 'prelims_explanation'
