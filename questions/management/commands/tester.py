7801778d5ad346cb20c356b9c6aa0c38c8c999c0
ankit anand <ankit.anand@ankits-MacBook-Air.local>
2025-03-02 02:50:32 +0530

    updating requirements.txt

--- questions/management/commands/tester.py
+++ questions/management/commands/tester.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 import logging
-from ...helpers import query_question_helper as helper
 logger = logging.getLogger(__name__)
 
 
@@ -8,5 +7,4 @@ class Command(BaseCommand):
     help = 'This is a utility management command for testing purpose'
 
     def handle(self, *args, **options):
-        helper.query_question()
         print("here")
